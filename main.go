2d4ac4b5f10ffd8f868861ce3d075d6625e3a55e
Crit Russell <crit.gfg@gmail.com>
2015-01-17 14:31:33 -0700

    working system

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"github.com/polds/MyIP"
+	"net/http"
 )
 
 func main() {
@@ -15,10 +16,45 @@ func main() {
 		Layout:     "layout",
 	}))
 
+	storer.InitDb()
+	cacher.InitCache()
+
+	m.Use(storageCheck())
+
 	m.Get("", func(out render.Render) {
 		ip, _ := myip.GetMyIP()
 		out.HTML(200, "index", ip)
 	})
 
+	m.Group("/api", func(m martini.Router) {
+
+		m.Get("/people", func(out render.Render) {
+			out.JSON(200, map[string][]Person{"people": PersonList()})
+		})
+
+		m.Put("/people", func(out render.Render, req *http.Request) {
+			name, email := req.FormValue("name"), req.FormValue("email")
+
+			if err := PersonCreate(name, email); err != nil {
+				out.JSON(500, err.Error())
+			} else {
+				out.Status(201)
+			}
+		})
+
+	})
+
+	m.NotFound(func(out render.Render) {
+		out.HTML(404, "404", nil)
+	})
+
 	m.Run()
+}
+
+func storageCheck() martini.Handler {
+	return func(out render.Render) {
+		if storer.MissingDb() {
+			out.HTML(500, "error", "Database is unavailable. Check server log for reason.")
+		}
+	}
 }
---
+++ person.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/mail"
+	"time"
+)
+
+type Person struct {
+	Id    int       `json:"id"`
+	Name  string    `json:"name"`
+	Email string    `json:"email"`
+	Date  time.Time `json:"date"`
+}
+
+func (Person) TableName() string {
+	return "people"
+}
+
+func PersonList() []Person {
+	list := make([]Person, 0)
+
+	cache := cacher.Get("people")
+
+	if err := json.Unmarshal([]byte(cache), &list); cache != "" && err == nil {
+		return list
+	}
+
+	db.Order("date desc").Find(&list)
+
+	if len(list) > 0 {
+		data, _ := json.Marshal(list)
+		cacher.Set("people", string(data))
+	}
+
+	return list
+}
+
+func PersonCreate(name, email string) error {
+	if name == "" {
+		return errors.New("Missing name from post.")
+	}
+
+	if email != "" && notEmail(email) {
+		return errors.New("Invalid email format.")
+	}
+
+	person := Person{Name: name, Email: email, Date: time.Now()}
+	err := db.Create(&person).Error
+
+	if err == nil {
+		cacher.Delete("people")
+	}
+
+	return err
+}
+
+func notEmail(in string) bool {
+	_, err := mail.ParseAddress(in)
+
+	return err != nil
+}
